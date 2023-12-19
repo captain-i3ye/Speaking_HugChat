577f37e1ad8fd34be10ce701dd066af08d2a7244
Aayush Ashok Kashyap <97963058+captain-i3ye@users.noreply.github.com>
2023-12-19 18:04:12 +0530

    Update hughug2.py

--- hughug2.py
+++ hughug2.py
@@ -4,33 +4,41 @@ from gtts import gTTS
 # from io import BytesIO
 import os
 import playsound
+#import time
 
-def signInAndChatHug(user: str, passwd: str, query: str) -> str:
-    cookie_path_dir = "./cookies_snapshot"
-    # if already logged in: loadCookiesFromDir else: get Cookies and save to Dir
-    sign = Login(user, passwd)
-    try:
-        cookies = sign.loadCookiesFromDir(cookie_path_dir)
-    except:
-        cookies = sign.login()
-        sign.saveCookiesToDir(cookie_path_dir)
+class hugC:
 
-    # create chatbot
-    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    def __init__(self) -> None:
+        self.user = "user_name"
+        self.passwd = "password"
+        self.cookie_dir_path = './cookies_snapshot'
+
+        self.sign = Login(self.user, self.passwd)
+        try:
+            self.cookies = self.sign.loadCookiesFromDir(self.cookie_dir_path)
+        except:
+            self.cookies = self.sign.login()
+            self.sign.saveCookiesToDir(self.cookie_dir_path)
+        
+        self.chatbot = hugchat.ChatBot(cookies=self.cookies.get_dict())
+
+        id = self.chatbot.new_conversation()
+        self.chatbot.change_conversation(id)
+
+        self.chatbot.switch_llm(1)
+
+    def chat(self, query: str) -> str:
+        result = self.chatbot.query(query, stream=False)
+        # result = str(result)
+        return result
     
-    # new conversation (for switching)
-    id = chatbot.new_conversation()
-    chatbot.change_conversation(id)
-
-    # switch models
-    chatbot.switch_llm(1)
-
-    # send query + retrieve result
-    result = chatbot.query(query, stream=True)
-    result = str(result)
-
-    chatbot.delete_all_conversations()
-    return result
+    def chatInfo(self) -> str:
+        self.info = self.chatbot.get_conversation_info()
+        print(self.info.id, self.info.title, self.info.model, self.info.system_prompt, self.info.history)
+    
+    def delChat(self):
+        print("Chat deleted\n")
+        self.chatbot.delete_all_conversations()
 
 def speakResult(text: str):
     tts = gTTS(text=text, lang='en')
@@ -39,11 +47,3 @@ def speakResult(text: str):
     tts.save(au_filename)
     playsound.playsound(au_filename)
     os.remove('./' + au_filename)
-
-query = input()
-result = signInAndChatHug(user, passwd, query)
-speakResult(result)
-
-
-
-
